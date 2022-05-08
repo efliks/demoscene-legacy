2ad96ffe27961f42a8d81ece0c2f3f0fbe6e088f
Mikolaj Feliks <mikolaj.feliks@gmail.com>
2022-05-08 23:12:36 +0200

    Extend graph library

--- asm/xfire/graphlib.asm
+++ asm/xfire/graphlib.asm
@@ -31,6 +31,30 @@ copy_screen proc
 endp
 
 
+copy_screen_fpu proc
+    push    ds
+    push    es
+
+    mov     ds, buffer_seg
+    xor     si, si
+    mov     ax, 0a000h
+    mov     es, ax
+    xor     di, di
+    mov     cx, 64000/8
+
+cb_copy:    
+    fld     qword ptr [si]
+    add     si, 8
+    fstp    qword ptr es:[di]
+    add     di, 8
+    dec     cx
+    jnz     cb_copy
+
+    pop     ds
+    pop     es
+endp
+
+
 clear_screen proc
     push    es
     mov     ax, buffer_seg
@@ -87,6 +111,32 @@ c2:
     in     al, dx
     test   al, 8
     jnz    c2
+    ret
+endp
+
+
+; ds:si -> input address to read palette
+set_palette proc
+    mov    dx, 03c8h
+    xor    ax, ax
+    out    dx, al
+    inc    dx
+    mov    cx, 768
+    cld
+    rep    outsb
+    ret
+endp
+
+
+; ds:di -> output address to write palette
+get_palette proc
+    mov    dx, 03c7h
+    xor    ax, ax
+    out    dx, al
+    add    dx, 2
+    mov    cx, 768
+    cld
+    rep    insb
     ret
 endp
 
